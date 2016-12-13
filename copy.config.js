d7ae8485c9a36ea5059100696e3600fa1d0358b2
JaeHo Song <thruthesky@gmail.com>
2016-12-13 15:46:13 +0900

    updating @ionic/app-scripts 0.0.47

--- copy.config.js
+++ copy.config.js
@@ -1,16 +1,17 @@
+// this is a custom dictionary to make it easy to extend/override
+// provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
+// then provide an object with a `src` array of globs and a `dest` string
 module.exports = {
-  include: [
-    {
-      src: '{{SRC}}/assets/',
-      dest: '{{WWW}}/assets/'
-    },
-    {
-      src: '{{SRC}}/index.html',
-      dest: '{{WWW}}/index.html'
-    },
-    {
-      src: '{{SRC}}/etc/hack/polyfills/polyfills.js',
-      dest: '{{BUILD}}/polyfills.js'
-    }
-  ]
-};
+  copyAssets: {
+    src: ['{{SRC}}/assets/**/*'],
+    dest: '{{WWW}}/assets'
+  },
+  copyIndexContent: {
+    src: ['{{SRC}}/index.html'],
+    dest: '{{WWW}}'
+  },
+  copyPolyfills: {
+    src: ['{{SRC}}/etc/hack/polyfills/polyfills.js'],
+    dest: '{{BUILD}}'
+  }
+}
