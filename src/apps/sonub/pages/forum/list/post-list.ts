1868a82718f909091a424217dcd64ab155abc91e
JaeHo Song <thruthesky@gmail.com>
2016-12-17 03:10:51 +0900

    work on speed improvment

--- src/apps/sonub/pages/forum/list/post-list.ts
+++ src/apps/sonub/pages/forum/list/post-list.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Post, PAGE, PAGES } from "../../../../../api/philgo-api/v2/post";
+import { Post, PAGE, POSTS } from "../../../../../api/philgo-api/v2/post";
 @Component({
     selector: 'sonub-post-list',
     templateUrl: 'post-list.html'
 })
 export class SonubPostListPage {
-    pages: PAGES = <PAGES> [];
+    // pages: PAGES = <PAGES> [];
+    posts: POSTS = <POSTS> [];
     constructor( private post: Post, activated: ActivatedRoute ) {
         let post_id = activated.snapshot.params['post_id'];
         post.page( { post_id: post_id }, (page: PAGE) => {
             console.log(page);
-            this.pages.push( page );
+            // this.posts = page.posts;
+            this.delayPush( page.posts );
         },
         error => alert(error)
         );
+    }
+
+    delayPush( arr ) {
+        arr.map( ( v, i ) => {
+            setTimeout( () => {
+                this.posts.push ( v );
+            },
+            i * 200 );
+        });
     }
 }
