54910190d9d4575cd31e22f6830948dd055d3883
JaeHo Song <thruthesky@gmail.com>
2016-12-16 21:44:02 +0900

    adding more forum. correcting forum listing with router parameter

--- src/apps/sonub/pages/forum/list/post-list.ts
+++ src/apps/sonub/pages/forum/list/post-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Post, PAGE, PAGES } from "../../../../../api/philgo-api/v2/post";
 @Component({
     selector: 'sonub-post-list',
@@ -6,8 +7,9 @@ import { Post, PAGE, PAGES } from "../../../../../api/philgo-api/v2/post";
 })
 export class SonubPostListPage {
     pages: PAGES = <PAGES> [];
-    constructor( private post: Post ) {
-        post.page( { post_id: 'qna' }, (page: PAGE) => {
+    constructor( private post: Post, activated: ActivatedRoute ) {
+        let post_id = activated.snapshot.params['post_id'];
+        post.page( { post_id: post_id }, (page: PAGE) => {
             console.log(page);
             this.pages.push( page );
         },
