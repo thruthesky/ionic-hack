f4c36a7a56f05eb05c6625ed097893ea2e5326df
JaeHo Song <thruthesky@gmail.com>
2016-12-17 23:20:36 +0900

    Customizing code for rendering speed

--- src/apps/sonub/pages/forum/list/post-list.ts
+++ src/apps/sonub/pages/forum/list/post-list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Post, PAGE, POSTS } from "../../../../../api/philgo-api/v2/post";
+import { Post, PAGE, POSTS, POST } from "../../../../../api/philgo-api/v2/post";
 @Component({
     selector: 'sonub-post-list',
     templateUrl: 'post-list.html'
@@ -11,8 +11,6 @@ export class SonubPostListPage {
     constructor( private post: Post, activated: ActivatedRoute ) {
         let post_id = activated.snapshot.params['post_id'];
         post.page( { post_id: post_id }, (page: PAGE) => {
-            console.log(page);
-            // this.posts = page.posts;
             this.delayPush( page.posts );
         },
         error => alert(error)
@@ -21,10 +19,17 @@ export class SonubPostListPage {
 
     delayPush( arr ) {
         arr.map( ( v, i ) => {
+            let s = 100;
+            if ( i > 10 ) s = 1000;
+            else if ( i > 6 ) s = 500;
             setTimeout( () => {
-                this.posts.push ( v );
+                this.posts.push ( this.pre(v) );
             },
-            i * 200 );
+            i * s );
         });
+    }
+    pre( post ) : POST {
+        post.url = "url";
+        return post;
     }
 }
