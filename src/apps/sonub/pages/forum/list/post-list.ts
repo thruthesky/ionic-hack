1df4b61f4b2d71d4139ae95a9c4cbaee8759b4b3
JaeHo Song <thruthesky@gmail.com>
2016-12-19 12:44:50 +0900

    Finishing up all vew/edit component. Refactoring codes

--- src/apps/sonub/pages/forum/list/post-list.ts
+++ src/apps/sonub/pages/forum/list/post-list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Post, PAGE, POSTS, POST } from "../../../../../api/philgo-api/v2/post";
+import { Post, PAGE, POSTS, POST, PAGE_OPTION } from "../../../../../api/philgo-api/v2/post";
 @Component({
     selector: 'sonub-post-list',
     templateUrl: 'post-list.html'
@@ -9,25 +9,61 @@ export class SonubPostListPage {
     // pages: PAGES = <PAGES> [];
     posts: POSTS = <POSTS> [];
     showPostCreateFrom: boolean = false;
+    post_id: string = '';
     constructor( private post: Post, activated: ActivatedRoute ) {
-        let post_id = activated.snapshot.params['post_id'];
+        this.post_id = activated.snapshot.params['post_id'];
 
-
-        post.page( { post_id: post_id, limit: 3 }, (page: PAGE) => {
-            if ( page.page_no == 1 ) this.posts = [];
-            this.delayPush( page.posts );
+        let option: PAGE_OPTION = {
+            post_id: this.post_id,
+            limit: 3
+        };
+        // post.debug = true;
+        post.page( option, (page: PAGE) => {
+            if ( page.page_no == 1 ) {
+                this.replacePush( page, option );
+                // this.removeFirstPage( option );
+            }
+            else this.delayPush( page );
         },
         error => alert(error)
         );
     }
 
-    delayPush( arr ) {
-        arr.map( ( v, i ) => {
+    delayPush( page:PAGE ) {
+        let posts = page.posts;
+        posts.map( ( v, i ) => {
             setTimeout( () => {
                 this.posts.push ( this.pre(v) );
             },
             100 + i * 10 );
         });
+    }
+    /**
+     * 이것은 글 하나씩 바꿔치기하므로 blinking 이 발생하지 않는다.
+     * 또한 첫 페이지에서 기존 글이 모두 올바로 제거된다.
+     * 새로운 글이 있으면 맨 아래의 기존 글은 다음페이지에 보이므로
+     * 그냥 해당 갯 수 만큼 바꿔치기 하면 된다.
+     * 요점 : 한꺼번에 한 페이지 전체를 바꿔치기 하지 않고, 약간의 시간차로 하나씩 바꿔치기 한다.
+     */
+    replacePush( page: PAGE, option: PAGE_OPTION ) {
+        for( let i = 0; i < option.limit; i ++ ) {
+            setTimeout( () => {
+                this.posts[i] = page.posts[i];
+            }, i * 100);
+        }
+    }
+    /**
+     * 이것은 그냥 첫 페이지 게시글을 한번에 없애버리므로 blinking 이 발생한다.
+     */
+    removeFirstPage( option: PAGE_OPTION ) {
+        // console.log("removeFirstPage() : ", option);
+        if ( this.posts.length == 0 ) {
+            // console.log("No post. just return");
+            return;
+        }
+        // console.log("before: ", this.posts.length);
+        this.posts.splice( 0, option.limit );
+        // console.log("after: ", this.posts.length);
     }
     pre( post ) : POST {
         post.url = this.post.getPermalink( post );
@@ -40,4 +76,9 @@ export class SonubPostListPage {
     editComponentOnCancel() {
         this.showPostCreateFrom = false;
     }
+    
+    editComponentOnSuccess() {
+        this.showPostCreateFrom = false;
+    }
+
 }
