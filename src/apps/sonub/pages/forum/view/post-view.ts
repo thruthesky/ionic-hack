ab7202b0741cf190c96c0eabd4039368070fc458
lancelynyrd <pinedaclp@gmail.com>
2016-12-22 17:47:18 +0800

    update post view

--- src/apps/sonub/pages/forum/view/post-view.ts
+++ src/apps/sonub/pages/forum/view/post-view.ts
@@ -11,22 +11,23 @@ export class SonubPostViewPage {
     idx_post: number = 0;
     post : POST_DATA;
 
-    constructor( 
+    constructor(
        private activated: ActivatedRoute,
        private postService : Post
          ) {
-        
+
         activated.params.subscribe( param => {
-            this.idx_post = param['idx_post']; 
-            this.loadPost(this.idx_post);   
+            this.idx_post = param['idx_post'];
+            this.loadPost(this.idx_post);
         });
     }
 
 
-    loadPost(idx_post){  
-        
+    loadPost(idx_post){
+
              this.postService.load(this.idx_post, response =>{
                  this.post = response.post;
+                 console.log("Load post : ", this.post );
                  console.log("Load post success on idx : ", idx_post);
             },error =>{
                 alert("Load post error" + error);
