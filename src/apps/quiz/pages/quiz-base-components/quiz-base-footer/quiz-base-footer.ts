bb5dfd0483f8ee91d27e7ebc975d457fb71ea7b5
aysonsteven <aysonsteven@gmail.com>
2016-12-22 14:58:06 +0800

    adding footer

---
+++ src/apps/quiz/pages/quiz-base-components/quiz-base-footer/quiz-base-footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { formProcess } from '../../../etc/share';
+@Component({
+    selector: 'quiz-base-footer',
+    templateUrl: 'quiz-base-footer.html'
+})
+export class QuizBaseFooter {
+    // process = formProcess;
+    constructor() {
+        // console.log("BaseFooter::constructor() ");
+    }
+}
